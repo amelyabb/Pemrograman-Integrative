abda8bceae27eff649b23717d0fb4be9d8ef2056
Amelya Batubara <80031312+amelyabb@users.noreply.github.com>
2021-03-04 16:24:21 +0700

    Menentukan bilangan negatif, positif, atau nol

---
+++ script.js
@@ -0,0 +1,17 @@
+alert('Program menentukan angka yang dimasukkan bilangan positif atau negatif');
+let angka = prompt('Masukan angka');
+
+if (angka >= 0) 
+  {
+    alert('Angka yang anda masukkan ini adalah bilangan positif');
+  } 
+
+  else if( angka == 0)
+  {
+    alert('Angka yang anda ini masukkan adalah 0');
+  } 
+
+  else (angka < 0) 
+  {
+    alert('Angka yang anda masukkan ini adalah bilangan negatif');
+  } 
