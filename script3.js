81aed7197ea2d57f98d910a1456a2c1688470ff8
Amelya Batubara <80031312+amelyabb@users.noreply.github.com>
2021-03-04 16:25:54 +0700

    Perulangan

---
+++ script3.js
@@ -0,0 +1,28 @@
+let status = true;
+while ( status = true ){  
+  alert('Program menentukan angka yang dimasukkan bilangan positif atau negatif')
+  let angka = parseInt(prompt('Masukan angka'))
+
+  if (angka >= 0) 
+  {
+    alert('Angka yang anda masukkan ini adalah bilangan positif');
+  } 
+
+  else if (angka == 0) 
+  {
+    alert('Angka yang anda ini masukkan adalah 0');
+  } 
+
+  else if (angka < 0) 
+  {
+    alert('Angka yang anda masukkan ini adalah bilangan negatif');
+  } 
+
+  else 
+  {
+    alert('Ini bukan angka');
+  }
+
+  tanya = confirm('Apakah anda mau memasukkan ulang angka?')
+  
+}
